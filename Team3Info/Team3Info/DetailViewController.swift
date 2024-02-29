83abd05c2bbe96fe1203631dd0f20c1a443be4f6
Heather-Cho <gmlfkgmlfk95@gmail.com>
2024-02-29 12:51:53 +0900

    IBOutlet 프로필뷰까지 생성

--- Team3Info/Team3Info/DetailViewController.swift
+++ Team3Info/Team3Info/DetailViewController.swift
@@ -9,13 +9,23 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
-    
+    //프로필 뷰
     @IBOutlet weak var profileView: UIView!
     
     @IBOutlet weak var profileImage: UIImageView!
     
     @IBOutlet weak var profileName: UILabel!
     
+    @IBOutlet weak var profileMail: UILabel!
+    
+    @IBOutlet weak var profileMailDetail: UILabel!
+    
+    @IBOutlet weak var profileBirth: UILabel!
+    
+    @IBOutlet weak var profileBirthDetail: UILabel!
+    
+    
+    //자기소개 뷰
     
     
     
