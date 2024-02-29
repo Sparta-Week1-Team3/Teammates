71bcf4cc1055931c0920be7502a0baa9b71c0838
Heather-Cho <gmlfkgmlfk95@gmail.com>
2024-02-29 15:34:44 +0900

    디테일뷰 머지

--- Team3Info/Team3Info/Controllers/DetailViewController.swift
+++ Team3Info/Team3Info/Controllers/DetailViewController.swift
@@ -8,17 +8,64 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
-
     
-    var memberData: Member?
+    
+    //프로필 뷰 IBOutlet
+    @IBOutlet weak var profileView: UIView!
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var profileName: UILabel!
+    
+    @IBOutlet weak var profileMail: UILabel!
+    
+    @IBOutlet weak var profileMailDetail: UILabel!
+    
+    @IBOutlet weak var profileBirth: UILabel!
+    
+    @IBOutlet weak var profileBirthDetail: UILabel!
+    
+    
+    
+    //어바웃 뷰 IBOutlet
+    @IBOutlet weak var aboutView: UIView!
+    
+    
+    
+    //자기소개 뷰 IBOutlet
+    @IBOutlet weak var introduceView: UIView!
+    
+    @IBOutlet weak var introduceTag: UILabel!
+    
+    @IBOutlet weak var introduceText: UILabel!
+    
+    
+    
+    //홈페이지 뷰 IBOutlet
+    @IBOutlet weak var homepageView: UIView!
+    
+    @IBOutlet weak var homepageVelogTitle: UILabel!
+    
+    @IBOutlet weak var homepageVelogText: UILabel!
+    
+    @IBOutlet weak var homepageGithubTitle: UILabel!
+    
+    @IBOutlet weak var homepageGithubText: UILabel!
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        profileView.layer.cornerRadius = 25
+        introduceView.layer.cornerRadius = 20
+        homepageView.layer.cornerRadius = 20
+        aboutView.layer.cornerRadius = 10
+        profileImage.backgroundColor = .lightGray
         
-        print("aaa")
+        
 
-        
     }
     
 
--- Team3Info/Team3Info/Models/DataManager.swift
+++ Team3Info/Team3Info/Models/DataManager.swift
@@ -13,7 +13,10 @@ class DataManager {
     
     func makeMemberData() {
         memberArray = [
+            //조현민 상세
             Member(profileImage: UIImage(named: "chynmn2"), memberName: "조현민", memberNickName: "@Chynmn", detailImage: UIImage(named: "chynmn_profile"), memberEmail: "chynmn0523@gmail.com", memberBirth: "05.23", memberTag: "#ENTJ", memberIntroduce: "안녕하세요~ 저는 협업의 중요성을 아느 iOS개발자가 되고자 합니다. 사교적인 성격이며, 최근에는 골프에 재미를 느껴 배우고 있습니다.", memberBlog: "https://chynmn.tistory.com", memberGithub: "https://github.com/Chynmn"),
+            
+            //
             Member(profileImage: UIImage(named: "chynmn3"), memberName: "조현민", memberNickName: "@Chynmn", detailImage: UIImage(named: "chynmn_profile"),memberEmail: "chynmn0523@gmail.com", memberBirth: "05.23", memberTag: "#ENTJ", memberIntroduce: "안녕하세요~ 저는 협업의 중요성을 아느 iOS개발자가 되고자 합니다. 사교적인 성격이며, 최근에는 골프에 재미를 느껴 배우고 있습니다.", memberBlog: "https://chynmn.tistory.com", memberGithub: "https://github.com/Chynmn"),
             Member(profileImage: UIImage(named: "chynmn2"), memberName: "조현민", memberNickName: "@Chynmn", detailImage: UIImage(named: "chynmn_profile"),memberEmail: "chynmn0523@gmail.com", memberBirth: "05.23", memberTag: "#ENTJ", memberIntroduce: "안녕하세요~ 저는 협업의 중요성을 아느 iOS개발자가 되고자 합니다. 사교적인 성격이며, 최근에는 골프에 재미를 느껴 배우고 있습니다.", memberBlog: "https://chynmn.tistory.com", memberGithub: "https://github.com/Chynmn"),
             Member(profileImage: UIImage(named: "chynmn3"), memberName: "조현민", memberNickName: "@Chynmn", detailImage: UIImage(named: "chynmn_profile"),memberEmail: "chynmn0523@gmail.com", memberBirth: "05.23", memberTag: "#ENTJ", memberIntroduce: "안녕하세요~ 저는 협업의 중요성을 아느 iOS개발자가 되고자 합니다. 사교적인 성격이며, 최근에는 골프에 재미를 느껴 배우고 있습니다.", memberBlog: "https://chynmn.tistory.com", memberGithub: "https://github.com/Chynmn"),
