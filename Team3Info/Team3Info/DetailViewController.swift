32346ac7f9073c34d5ae853a6726b9f9e3997a7c
Heather-Cho <gmlfkgmlfk95@gmail.com>
2024-02-29 13:04:04 +0900

    cornerRadius추가 완료

--- Team3Info/Team3Info/DetailViewController.swift
+++ Team3Info/Team3Info/DetailViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
-    //프로필 뷰
+    
+    //프로필 뷰 IBOutlet
     @IBOutlet weak var profileView: UIView!
     
     @IBOutlet weak var profileImage: UIImageView!
@@ -25,14 +26,31 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var profileBirthDetail: UILabel!
     
     
-    //자기소개 뷰
+    
+    //어바웃 뷰 IBOutlet
+    @IBOutlet weak var aboutView: UIView!
     
     
     
+    //자기소개 뷰 IBOutlet
+    @IBOutlet weak var introduceView: UIView!
+    
+    @IBOutlet weak var introduceTag: UILabel!
+    
+    @IBOutlet weak var introduceText: UILabel!
     
     
     
+    //홈페이지 뷰 IBOutlet
+    @IBOutlet weak var homepageView: UIView!
     
+    @IBOutlet weak var homepageVelogTitle: UILabel!
+    
+    @IBOutlet weak var homepageVelogText: UILabel!
+    
+    @IBOutlet weak var homepageGithubTitle: UILabel!
+    
+    @IBOutlet weak var homepageGithubText: UILabel!
     
     
     
@@ -40,8 +58,14 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        profileView.layer.cornerRadius = 25
+        introduceView.layer.cornerRadius = 20
+        homepageView.layer.cornerRadius = 20
+        aboutView.layer.cornerRadius = 10
+        profileImage.backgroundColor = .lightGray
         
+        
+
     }
     
 
